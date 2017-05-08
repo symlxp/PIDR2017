1761b6652cda3b2be5b6d2a6ba6b4db80346ffda
i3yaki <jacky.ho@telecomnancy.net>
2017-05-08 14:45:33 +0200

    Add files via upload

---
+++ right.sh
@@ -0,0 +1,15 @@
+
+# Usage: install-pin-on-device.sh
+#
+#    Expect 1 emulator running
+
+
+
+# on prend les droits root
+chmod 775 ./scripts/install.sh
+chmod 775 ./scripts/create-devices.sh
+chmod 775 ./scripts/get-pin.sh
+chmod 775 ./scripts/get-android-sdk.sh
+chmod 775 ./busybox/busybox
+chmod 775 ./busybox/busybox_TAR
+
