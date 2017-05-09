adedb0876432b0d8ead2bb98149a44432467cf1f
i3yaki <jacky.ho@telecomnancy.net>
2017-05-09 22:48:26 +0200

    change and edit readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# PIDR2017
+## PIDR2017
+
+# ETAPE 1
+git clone https://github.com/tristanvdb/pin-android.git
+cd PIDR2017
+
+Si vous possédez déjà un émulateur android, passez directement à l'étape 3.
--- inscount-apk.sh
+++ inscount-apk.sh
@@ -8,8 +8,10 @@ pkg=$(aapt dump badging $1|awk -F" " '/package/ {print $2}'|awk -F"'" '/name=/ {
 act=$(aapt dump badging $1|awk -F" " '/launchable-activity/ {print $2}'|awk -F"'" '/name=/ {print $2}')
 # on prend les droits root
 ./adb root
-./adb install "./busybox/SansChargement.apk"
+./adb push ./inscount /data/tools/pintool/
+./adb install "./inscount/SansChargement.apk"
 ./adb shell "cd /data/tools/pintool ; ./pin -t ./inscount2.so -- am start -n $pkg/$act"
+sleep 3
 ./adb pull ./data/tools/pintool/inscount.out
 echo "--------------------------------------"
 echo "Fin du script"
